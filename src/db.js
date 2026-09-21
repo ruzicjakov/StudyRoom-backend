130fdf7c6ac6c6439908cd6233bd385a22fdd88a
Jakov Ružić <jbenedikt1011@gmail.com>
2026-09-21 22:32:16 +0200

    spajanje s vercelom

--- src/db.js
+++ src/db.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   },
   waitForConnections: true,
   connectionLimit: 10,
